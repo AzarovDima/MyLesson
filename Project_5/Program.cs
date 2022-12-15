046bd912059f7cac7948e997039ffbf7338b81c3
AzarovDima <ada89885308140@gmail.com>
2022-12-16 00:01:04 +0300

    Сделали 5 проект

---
+++ Project_5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Приветствуем по разному
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Дима")
+{
+    Console.WriteLine("Ура, это же Дима!!! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
