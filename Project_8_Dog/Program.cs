aee3b4adfb82e806457a7425de475045bc36a3c8
AzarovDima <ada89885308140@gmail.com>
2022-12-17 13:00:21 +0300

    Сколько раз бегает собака между друзьями

---
+++ Project_8_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿// ПОстчитать сколько раз собака пробежит между друзьями
+
+int count = 0;
+int distanse = 10000;
+int firstFrendSpeed = 1;
+int secondFrendSpeed = 2;
+int dogSped = 4;
+int friend = 1;
+friend == 2;
+
+while (distanse > 5)
+{
+    if (friend == 1)
+    {
+        t = distanse / (firstFrendSpeed + dogSped);
+        friend == 2;
+    }
+    else (friend = 2);
+    {
+        t = distanse / (secondFrendSpeed + dogSped);
+        friend == 1;
+    }
+    distanse = distanse - (firstFrendSpeed + secondFrendSpeed) * t;
+    count++;
+}
+Console.WriteLine(count);
+
+
+
