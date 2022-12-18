52ed9e1a2157f3947e86383c8390db5d50b0ef36
AzarovDima <ada89885308140@gmail.com>
2022-12-18 21:46:28 +0300

    Решаем 12 задачу

---
+++ Project_12_ArrayLibrary/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void FillArray(int[collection])
+{
+    int length = CollectionExtensions.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Collection[index]=new Random().Next(1,10);
+        index++;
+    }
+}
+
+
+
