68b27aa5838a1756adbb99ec4ad9800097915eff
AzarovDima <ada89885308140@gmail.com>
2022-12-18 21:38:06 +0300

    Решили 11 задачу с Массивом

---
+++ Project_11_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//              0   1   2   3  4   5   6   7   8   9  10  11  12  13  14  15  16 
+int[] array = { 23, 83, 31, 6, 4, 21, 73, 83, 12, 56, 21, 31, 43, 84, 342, 13, 83 };
+
+int n = array.Length;
+int find = 83;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
