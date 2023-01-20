649449c306ccca1bb7f430e6b1d9c0f6acfff7ec
AzarovDima <ada89885308140@gmail.com>
2023-01-20 22:27:09 +0300

    3 лекция

---
+++ Lec_13_Metods/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Метод 1  // Метод который ничего не возвращает и не принимает
+void Method1()
+{
+    Console.WriteLine("Автор Д.А. Азаров!!!");
+}
+//Method1();
+
+// Метод 2 // Методы которые не возвращают а принимают что либо.
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method1(msg: "Текст сообщения");
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текст", count: 4);
+//Method21(count: 4, msg: "новый текст");
+
+// Метод 3  // Методы которые возвращают что-либо.
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+
+// Метод 4  // Методы которые возвращают что либо и принимают что либо
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "Это ");
+Console.WriteLine(res);
+
---
+++ Lec_5_if_else/Program.cs
@@ -0,0 +1,15 @@
+﻿// Приветствуем по разному
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine() ?? "";
+
+if (username.ToLower() == "дима")
+{
+    Console.WriteLine("Ура, это же ДИМА!!! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
