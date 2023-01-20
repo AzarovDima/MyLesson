489fb223c6493241e1c9d1833c14443a863f2599
AzarovDima <ada89885308140@gmail.com>
2023-01-20 23:58:01 +0300

    Закончили 3 лекцию

--- Lec_16_funkc/Program.cs
+++ Lec_16_funkc/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Дан текст. В тексте нужно все пробелы заменить черточками,
+маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+маленькими “с”. */
+
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, 'с', 'С');
+Console.WriteLine(newText);
---
+++ Lec_17_sort/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*Упорядочить массивы
+Было: 6 8 3 2 1 4 5 7
+Стало: 1 8 3 2 6 4 5 7 */
+
+int[] arr = { 5, 6, 8, 2, 6, 4, 3, 9, 2, 7, 0 };
+
+
+void PtintArray(int[] array1)
+{
+    int count = array1.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
+void SwlectionSort(int[] array2)
+{
+    for (int i = 0; i < array2.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array2.Length; j++)
+        {
+            if (array2[j] < array2[minPosition]) minPosition = j;
+        }
+        int temporary = array2[i];
+        array2[i] = array2[minPosition];
+        array2[minPosition] = temporary;
+    }
+}
+
+PtintArray(arr);
+SwlectionSort(arr);
+PtintArray(arr);
