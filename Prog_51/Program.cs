783c26c263527bb3d44bf750bfc35478e5564e9a
AzarovDima <ada89885308140@gmail.com>
2023-01-29 22:45:30 +0300

    Решили 54 задачу

--- Prog_51/Program.cs
+++ Prog_51/Program.cs
@@ -98,12 +98,35 @@ void PrintArray(int[,] inArray)
 } */
 
 
-/* Console.Clear();
-
-int[,] array = FillArray(6, 6, 0, 10);
+/* int[,] array = GetArray(6, 6, 0, 10);
 PrintArray(array);
 CheckArray(array, 0, 10);
 
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 void CheckArray(int[,] inArray, int minValue, int maxValue)
 {
---
+++ Prog_54_Sort/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+
+Console.WriteLine("Печатаю массив...");
+int[,] inArray = GetArray(5, 6, 0, 10);
+PrintArray(inArray);
+int[,] sortArray = SotrArray(inArray);
+Console.WriteLine("Ниже получили новый массив с сортировкой по строкам от большего к меньшему");
+PrintSortArray(inArray);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SotrArray(int[,] array)
+{
+    int[,] newresult = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++) // новая переменная, которая будет обходить строку и сортировать
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return newresult;
+}
+void PrintSortArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
