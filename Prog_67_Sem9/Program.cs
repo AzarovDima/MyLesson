eff761f03850b9c50955c9c69795bc68d4f358a4
AzarovDima <ada89885308140@gmail.com>
2023-02-06 21:52:27 +0300

    Решили все задачи 9 семинара

--- Prog_67_Sem9/Program.cs
+++ Prog_67_Sem9/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+
+int userNumber = GetNumbetFromUser("Введите натуральное число ", " Ошибка ввода!");
+int result = Recurse(userNumber);
+
+Console.Write($"{userNumber} -> {result}");
+
+int GetNumbetFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int Recurse(int userNumber)
+{
+    if (userNumber == 0) return 0;
+    else
+    {
+        return userNumber % 10 + Recurse(userNumber / 10);
+    }
+}
--- Prog_69_Sem9/Program.cs
+++ Prog_69_Sem9/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+
+int userNumberA = GetNumbetFromUser("Введите натуральное число A: ", " Ошибка ввода!");
+int userNumberB = GetNumbetFromUser("Введите натуральное число B: ", " Ошибка ввода!");
+
+int result = Recurse(userNumberA, userNumberB);
+
+Console.Write($"A={userNumberA}; B={userNumberB} -> {result}");
+
+int GetNumbetFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int Recurse(int userNumberA, int userNumberB)
+{
+    int result = 1;
+    if (userNumberB == 0) return 1;
+    if (userNumberA == 0) return 0;
+    else
+    {
+        result = userNumberA * Recurse(userNumberA, userNumberB - 1);
+        return result;
+    }
+}
+
+
