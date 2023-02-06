c1bcb537eaf2666f2e304dfa0abd095a368a0083
AzarovDima <ada89885308140@gmail.com>
2023-02-06 21:20:19 +0300

    9 Семинар

---
+++ Prog_63++Sem9/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+int userNumber = GetNumbetFromUser("Введите целоечисло: ", " Ошибка ввода!");
+
+int GetNumbetFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+Console.Write($"N = {userNumber} -> ");
+
+string ShowLine(int start, int userNumber)
+{
+    if (start == userNumber) return userNumber.ToString();
+    else return start + ", " + ShowLine(start + 1, userNumber);
+}
+
+Console.Write(ShowLine(1, userNumber));
+
+// https://boosty.to/ksergeyru/posts/e70dec1b-972b-4a91-b247-06b790de97a1?share=post_link
+
+
---
+++ Prog_65_sem9/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8" */
+
+int userNumber1 = GetNumbetFromUser("Введите 1 число: ", " Ошибка ввода!");
+int userNumber2 = GetNumbetFromUser("Введите 2 число: ", " Ошибка ввода!");
+int GetNumbetFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+Console.Write($"M={userNumber1}; N={userNumber2} -> ");
+
+string ShowLine(int start, int end)
+{
+    if (start == end) return end.ToString();
+    else return start + ", " + ShowLine(start + 1, end);
+}
+
+if (userNumber1 < userNumber2) Console.Write(ShowLine(userNumber1, userNumber2));
+else Console.Write(ShowLine(userNumber2, userNumber1));
